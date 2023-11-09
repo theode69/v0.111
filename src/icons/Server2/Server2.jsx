e69e46470e4d05ea86dc8ff751e35a2cd6bf3e6c
theo__0101 <43608531+theode69@users.noreply.github.com>
2023-11-09 13:49:43 +0530

    Initial commit

---
+++ src/icons/Server2/Server2.jsx
@@ -0,0 +1,49 @@
+/*
+We're constantly improving the code you see. 
+Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
+*/
+
+import React from "react";
+
+export const Server2 = ({ className }) => {
+  return (
+    <svg
+      className={`server-2 ${className}`}
+      fill="none"
+      height="22"
+      viewBox="0 0 22 22"
+      width="22"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g className="g" clipPath="url(#clip0_34_18937)">
+        <path
+          className="path"
+          d="M18.1001 2.15991H4.06006C3.0908 2.15991 2.30505 2.94565 2.30505 3.91492V7.42493C2.30505 8.39419 3.0908 9.17994 4.06006 9.17994H18.1001C19.0694 9.17994 19.8551 8.39419 19.8551 7.42493V3.91492C19.8551 2.94565 19.0694 2.15991 18.1001 2.15991Z"
+          stroke="#C0C0C0"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.75501"
+        />
+        <path
+          className="path"
+          d="M18.1001 12.6899H4.06006C3.0908 12.6899 2.30505 13.4757 2.30505 14.4449V17.955C2.30505 18.9242 3.0908 19.71 4.06006 19.71H18.1001C19.0694 19.71 19.8551 18.9242 19.8551 17.955V14.4449C19.8551 13.4757 19.0694 12.6899 18.1001 12.6899Z"
+          stroke="#C0C0C0"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.75501"
+        />
+      </g>
+      <defs className="defs">
+        <clipPath className="clip-path" id="clip0_34_18937">
+          <rect
+            className="rect"
+            fill="white"
+            height="21.0601"
+            transform="translate(0.550049 0.404907)"
+            width="21.0601"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
